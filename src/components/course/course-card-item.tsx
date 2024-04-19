a1bd10e1fb1ab9da89278db30f8e4c0e94a20f24
omrajsharma <omraj7413@gmail.com>
2024-04-19 10:38:58 +0530

    chore: program we offer data correction

--- src/components/course/course-card-item.tsx
+++ src/components/course/course-card-item.tsx
@@ -59,7 +59,7 @@ const CourseCardItem: FC<Props> = ({ item }) => {
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Typography variant="h5" color="primary.main">
-              {'$' + item.price}
+              {'₹' + item.price.toLocaleString()}
             </Typography>
             <Typography variant="h6">/ course</Typography>
           </Box>
--- src/components/home/popular-courses.tsx
+++ src/components/home/popular-courses.tsx
@@ -102,7 +102,7 @@ const HomePopularCourse: FC = () => {
               }}
             >
               <Typography variant="h1" sx={{ mt: { xs: 0, md: -5 }, fontSize: { xs: 30, md: 48 } }}>
-                Most Popular Courses
+                Programs We Offer
               </Typography>
             </Box>
           </Grid>
