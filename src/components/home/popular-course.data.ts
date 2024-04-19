9d3d5ad27c38ed07c06b90a99aaf75221ad730a5
omrajsharma <omraj7413@gmail.com>
2024-04-19 10:44:30 +0530

    refactor: code clean up

--- src/components/home/popular-course.data.ts
+++ src/components/home/popular-course.data.ts
@@ -36,32 +36,5 @@ export const data: Array<Course> = [
     ratingCount: 257,
     price: 69999,
     category: 'Beginner',
-  },
-  // {
-  //   id: 5,
-  //   cover: '/images/courses/stillness-inmotion-Jh6aQX-25Uo-unsplash.jpg',
-  //   title: 'Modern React with MUI & Redux',
-  //   rating: 4,
-  //   ratingCount: 32,
-  //   price: 35,
-  //   category: 'Intermediate',
-  // },
-  // {
-  //   id: 6,
-  //   cover: '/images/courses/stillness-inmotion-YSCCnRGrD-4-unsplash.jpg',
-  //   title: 'Ethical Hacking Bootcamp Zero to Mastery',
-  //   rating: 5,
-  //   ratingCount: 14,
-  //   price: 35,
-  //   category: 'Beginner',
-  // },
-  // {
-  //   id: 7,
-  //   cover: '/images/courses/grovemade-RvPDe41lYBA-unsplash.jpg',
-  //   title: 'Adobe Lightroom For Beginners: Complete Photo Editing',
-  //   rating: 4,
-  //   ratingCount: 6,
-  //   price: 25,
-  //   category: 'Beginner',
-  // },
+  }
 ]
