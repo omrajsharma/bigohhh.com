1a82d40567ea9416b02d92bb6d8bb6e46d89c255
neevgit25 <niveditakar88@gmail.com>
2024-05-19 22:22:21 +0530

    changed Get Started and Subscribe button color while hovering

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
