8ae55148f8be7a3b93953bb993008da42d98f5c3
Ananya <ananya.dpssirsa123@gmail.com>
2024-05-27 16:28:27 +0530

    fixed scrollToTop Button

--- client/src/components/home/scrollToTop.tsx
+++ client/src/components/home/scrollToTop.tsx
@@ -1,56 +1,50 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-"use client"
-import { useEffect, useState } from "react";
-import { ChevronUp } from "lucide-react"
-import {StyledButton } from "../styled-button"
+
+'use client'
+import { useEffect, useState, CSSProperties } from 'react'
+import { ChevronUp } from 'lucide-react'
+import { StyledButton } from '../styled-button'
 
 const ScrollToTopButton = () => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false)
 
   useEffect(() => {
     const toggleVisibility = () => {
-      window.scrollY > 500 ? setIsVisible(true) : setIsVisible(false);
-    };
-    window.addEventListener("scroll", toggleVisibility);
+      window.scrollY > 500 ? setIsVisible(true) : setIsVisible(false)
+    }
+    window.addEventListener('scroll', toggleVisibility)
 
     return () => {
-      window.removeEventListener("scroll", toggleVisibility);
-    };
-  }, []);
-
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
 
   const scrollToTop = () => {
     if (isVisible) {
       window.scrollTo({
         top: 0,
-        behavior: "smooth",
-      });
+        behavior: 'smooth',
+      })
     }
-  };
+  }
+
+  //scroll to top button style
+  const buttonStyles: CSSProperties = {
+    position: 'fixed',
+    bottom: '16px',
+    right: '16px',
+    zIndex: 1000,
+    opacity: isVisible ? 1 : 0,
+    transition: 'opacity 0.2s ease-in-out',
+  }
 
   return (
-    
-    <StyledButton
-      onClick={scrollToTop}
-      color="dark"
-      variant="outlined"
-      size="medium"
-      sx={{
-        position: 'fixed',
-        bottom: '16px',
-        right: '16px',
-        zIndex: 1000,
-        opacity: isVisible ? 1 : 0,
-        transition: 'opacity 0.2s ease-in-out',
-        
-      }}
-      startIcon={<ChevronUp />}
-    >
+    <div style={buttonStyles}>
+      <StyledButton onClick={scrollToTop} color="dark" variant="outlined" size="medium" startIcon={<ChevronUp />}>
+        Scroll to Top
+      </StyledButton>
+    </div>
+  )
+}
 
-    </StyledButton>
-     
-    
-  );
-};
-
-export default ScrollToTopButton;
+export default ScrollToTopButton
