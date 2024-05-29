3f0acf247c6f4c97ee7ff0b2c23cb89ec2588421
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-05-29 23:12:08 +0530

    Feat/User can login with google id.

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import IconButton from '@mui/material/IconButton'
@@ -7,14 +7,50 @@ import { Logo } from '@/components/logo'
 import { Navigation, AuthNavigation } from '@/components/navigation'
 import { useTheme } from '@mui/material/styles'
 import { Menu, Close } from '@mui/icons-material'
+import Button from '@mui/material/Button'
+import Signup from '../sign-up/SignUp'
+import Avatar from 'react-avatar';
+import { onAuthStateChanged, getAuth } from "firebase/auth";
+import {app, signOutUser} from '../../firebase/firebaseConf';
 
 const Header: FC = () => {
   const [visibleMenu, setVisibleMenu] = useState<boolean>(false)
   const { breakpoints } = useTheme()
   const matchMobileView = useMediaQuery(breakpoints.down('md'))
   const toggleMenu: () => void = () => {
-    setVisibleMenu(visibleMenu => !visibleMenu);
-};
+    setVisibleMenu((visibleMenu) => !visibleMenu)
+  }
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [showSignUp, setShowSignUp] = useState<boolean>(false)
+  const [userData, setUserData] = useState({
+    displayName: '',
+    email: '',
+  })
+  useEffect(() => {
+    const auth = getAuth(app);
+    const unsubscribe = onAuthStateChanged(auth, (result) => {
+      if (result) {
+
+        const {displayName, email} = result;
+        const userEmail = email ?? '';
+        const userName = displayName ?? 'Anonymous';
+        
+        setUserData({ displayName: userName, email: userEmail });
+
+        setIsLoggedIn(true)
+      } else {
+        setIsLoggedIn(false)
+      }
+
+    })
+
+    return () => unsubscribe();
+  },[])
+
+  const handleUserLogin = (): void => {
+    setShowSignUp(!showSignUp)
+  }
+
   return (
     <Box sx={{ backgroundColor: 'background.paper', position: 'sticky', top: 0, zIndex: 1000 }}>
       <Container sx={{ py: { xs: 2, md: 3 } }}>
@@ -46,7 +82,7 @@ const Header: FC = () => {
             }}
           >
             <Box /> {/* Magic space */}
-            <Navigation toggleMenu={toggleMenu}/>
+            <Navigation toggleMenu={toggleMenu} />
             <AuthNavigation />
             {visibleMenu && matchMobileView && (
               <IconButton
@@ -61,6 +97,21 @@ const Header: FC = () => {
               </IconButton>
             )}
           </Box>
+          {!isLoggedIn && (
+            <Button variant="contained" href="#contained-buttons" onClick={handleUserLogin}>
+              Login
+            </Button>
+          )}
+          {isLoggedIn && userData.displayName && (
+      
+              <Avatar name={userData.displayName}  size="40" round="50%" />
+          ) }
+          <Signup
+            isShow={showSignUp}
+            returnShow={setShowSignUp}
+            returnLoggedIn={setIsLoggedIn}
+            returnUserData={setUserData}
+          />
         </Box>
       </Container>
     </Box>
---
+++ client/src/firebase/firebaseConf.ts
@@ -0,0 +1,79 @@
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+  Auth,
+  UserCredential,
+} from "firebase/auth";
+import "firebase/compat/auth";
+import "firebase/compat/database";
+import { FirebaseApp, FirebaseOptions, initializeApp } from "firebase/app";
+import { getFirestore, Firestore } from "firebase/firestore";
+import { getDatabase, Database } from "firebase/database";
+import { getStorage, FirebaseStorage } from "firebase/storage";
+import { Zoom, toast } from "react-toastify";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyAlMyYap3xZieudHQxppcjTHMNZYQPQdZg",
+  authDomain: "bigohhh-dc6d2.firebaseapp.com",
+  projectId: "bigohhh-dc6d2",
+  storageBucket: "bigohhh-dc6d2.appspot.com",
+  messagingSenderId: "952055932719",
+  appId: "1:952055932719:web:7e06ff41f0e2d265e4a3da",
+  measurementId: "G-CE2MLEGGH6"
+};
+
+
+const app: FirebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Firebase authentication
+const auth: Auth = getAuth(app);
+
+// Initialize Firestore database
+const firestore: Firestore = getFirestore(app);
+
+// Initialize Realtime Database
+const database: Database = getDatabase(app);
+
+// Initialize Firebase storage
+const storage: FirebaseStorage = getStorage(app);
+
+const provider: GoogleAuthProvider = new GoogleAuthProvider();
+
+// Whenever a user interacts with the provider, we force them to select an account
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+const signInWithGooglePopup = async (): Promise<UserCredential> => {
+  try {
+    return await signInWithPopup(auth, provider);
+  } catch (error) {
+    console.error(error);
+    throw error; // Optional: rethrow the error if you want to handle it outside
+  }
+};
+
+const signOutUser = async (): Promise<void> => {
+  try {
+    await signOut(auth);
+    localStorage.clear();
+    toast.success("Logged out successfully", { transition: Zoom });
+    window.location.href = "#/";
+    window.location.reload();
+  } catch (error) {
+    console.error(error);
+    throw error; // Optional: rethrow the error if you want to handle it outside
+  }
+};
+
+export {
+  auth,
+  firestore,
+  database,
+  storage,
+  app,
+  signInWithGooglePopup,
+  signOutUser,
+};
